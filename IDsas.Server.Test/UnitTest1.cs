20fca65fbc9e50d2db5c2b08cad22d52d64f0501
Gints K. Kulikovskis <gints.kulikovskis@gmail.com>
2024-10-20 20:26:55 +0200

    Implement basic pipeline

--- IDsas.Server.Test/UnitTest1.cs
+++ IDsas.Server.Test/UnitTest1.cs
@@ -1,11 +1,11 @@
-namespace IDsas.Server.Test
-{
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
-    }
-}
+//namespace IDsas.Server.Test
+//{
+//    [TestClass]
+//    public class UnitTest1
+//    {
+//        [TestMethod]
+//        public void TestMethod1()
+//        {
+//        }
+//    }
+//}
