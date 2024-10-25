d1abe4c52d3504ab95f429eb337f5d424545e544
Gints K. Kulikovskis <gints.kulikovskis@gmail.com>
2024-10-25 16:02:56 +0200

    Add "associated user" property to Link entity

--- IDsas.Server/Entities/Document.cs
+++ IDsas.Server/Entities/Document.cs
@@ -3,8 +3,9 @@ namespace IDsas.Server.Entities;
 
 public class Document
 {
-    public string Id { get; set; }
+    public int Id { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
+    public User Author { get; set; }
     public IList<DocumentLink> DocumentLinks { get; set; }
 }
