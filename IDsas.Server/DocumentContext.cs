6991796926c4099be9b11bd2ec514a20f83f8b2d
Gints K. Kulikovskis <gints.kulikovskis@gmail.com>
2024-10-25 15:59:09 +0200

    Add EFCore nuget package references

---
+++ IDsas.Server/DocumentContext.cs
@@ -0,0 +1,15 @@
+﻿using IDsas.Server.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace IDsas.Server
+{
+    public class DocumentContext : DbContext
+    {
+        public DbSet<Document> Documents { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=documents.db");
+        }
+    }
+}
